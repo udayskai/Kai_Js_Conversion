21dace2ee509ba19e775ee2ec9e47faa65dc3187
uday singh <[udayskai@gmail.com]>
2019-10-01 23:21:25 +0530

    conversion

---
+++ main.js
@@ -0,0 +1,35 @@
+"use strict"
+
+//Number to String
+
+let Number_Value=50;
+let String_Value=String(Number_Value);
+console.log(String_Value);
+
+//Number to Boolean
+ 
+let Bool_Value=Boolean(Number_Value);
+console.log(Bool_Value);
+
+//Boolean to Number
+
+let Bool_Value1=false;
+let Number_value1=Number(Bool_Value1);
+console.log(Number_value1);//ture=1 and false=0
+
+// boolean to String
+
+let String_Value1=String(Bool_Value1);
+console.log(String_Value1);
+
+
+//String to Number
+
+let String_Value2="Name";
+let Number_value2=Number(String_Value2); // for word its "NAN" & for Number is Number/
+console.log(Number_value2);
+
+//string to boolean
+
+let Bool_Value2=Boolean(String_Value2);
+console.log(Bool_Value2);
